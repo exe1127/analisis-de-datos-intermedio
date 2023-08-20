7c0483c0056b44f4e90356fe9bfb50c6f8543a08
Exequiel Zalazar <exequiel.zalazar@nonerds.com>
2023-08-20 19:24:56 -0300

    se completo el normalizado

--- function.py
+++ function.py
@@ -3,7 +3,7 @@ from config import *
 from datetime import datetime
 import pandas as pd
 from pandas import json_normalize
-
+import json
 
 """ def search(city, key):
 
@@ -17,36 +17,50 @@ from pandas import json_normalize
         raise Exception('Error searching status code', request.status_code) """
 
 
-def search2(lat, lon, key, dt):
+def search2(lat, lon, key):
     # Consulta a la appi
-    url = f'{baseUrl}lat={lat}&lon={lon}&dt={dt}&appid={key}&units=metric'
+    url = f'{baseUrl}lat={lat}&lon={lon}&appid={key}&units=metric'
 
     request = requests.get(url)
+
     if request.status_code == 200:
         return request.json()
     else:
         raise Exception('Error searching status code', request.status_code)
 
 
-def normalize(request,index):
+def normalize(request):
     # Normalizacion de la respuesta de la api
     datecols = ['dt', 'sunrise', 'sunset']
-    records = []
 
-    # Utiliza json_normalize para extraer y normalizar los datos
-    star = json_normalize(request)
-    star = star.drop("data", axis=1)
-    weather = json_normalize(request["data"][0]["weather"][0])
-    data = json_normalize(request['data'][0])
-    data = data.drop("weather", axis=1)
+    # Aplanar el JSON para combinar "dt", "city_name" y subcampos de "main"
+    extracted_data = []
 
-    # Se genera el dataFrame correspondiente
-    df = pd.concat([star, data, weather,index], axis=1)
+    for item in request['list']:
+        main_data = item['main']
+        extracted_item = {
+            'dt': item['dt'],
+            'city_name': data['city']['name'],
+            'temp': main_data['temp'],
+            'feels_like': main_data['feels_like'],
+            'temp_min': main_data['temp_min'],
+            'temp_max': main_data['temp_max'],
+            'pressure': main_data['pressure'],
+            'sea_level': main_data['sea_level'],
+            'grnd_level': main_data['grnd_level'],
+            'humidity': main_data['humidity'],
+            'temp_kf': main_data['temp_kf']
+        }
+        extracted_data.append(extracted_item)
 
-    # Establece el índice del DataFrame en la columna 'id' para evitar errores
+# Crear el DataFrame a partir de la información extraída
+    df = pd.DataFrame(extracted_data)
+
+    """  records.append(df) """
+
+    """ # Establece el índice del DataFrame en la columna 'id' para evitar errores
     df = df.set_index('index')
-
-    # Convierte las columnas de fechas a objetos de fecha y hora
-    df[datecols] = df[datecols].apply(pd.to_datetime)
-
+ """
+    """  # Convierte las columnas de fechas a objetos de fecha y hora
+    df[datecols] = df[['dt']].apply(pd.to_datetime) """
     return df
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from function import *
 import datetime
 if __name__ == '__main__':
     # Obtener del archivo txt la key
-    with open('credenciales.txt', 'r') as f:
+    with open('credencial.txt', 'r') as f:
         key = f.read()
 
     coordList = ["lat=31&lon=64", "lat=40&lon=-73", "lat=-31&lon=-64", "lat=25&lon=64", "lat=-34&lon=-58",
@@ -13,24 +13,22 @@ if __name__ == '__main__':
     list = []
     tiempo_actual = datetime.datetime.now()
 
-    # Realizamos la primera busqueda por nombre de la ciudad
-
+    # Realizamos la busqueda por coordenadas 
     for coords in coordList:
         # Separamos los elementos del arrregolo en longitud y latitud
         lat, lon = coords.split('&')
     # Extraemos los valores numéricos de latitud y longitud después de "lat=" y "lon="
         lat = float(lat.split('=')[1])
         lon = float(lon.split('=')[1])
-        for i in range(0, 4):
-            tiempo_unix = int(
-                (tiempo_actual-datetime.timedelta(days=i)).timestamp())
-    # Realizamos la segunda busqueda por latitud y longitud de la ciudad
-            list.append(search2(lat, lon, key, tiempo_unix))
+        """ # Realizamos la segunda busqueda por latitud y longitud de la ciudad
+        list.append(search2(lat, lon, key)) """
+
+    list.append(data)
 
     # Se genera la normalizacion de la lista obtenida en las busquedas
-    normalized_data = [normalize(entry,len(list)) for entry in list]
+    normalized_data = [normalize(entry) for entry in list]
 
-    # Se consolida en un dataFrame
-    consolidated_df = pd.concat(normalized_data)
+    """ # Se consolida en un dataFrame
+    consolidated_df = pd.concat(normalized_data) """
 
-    print(consolidated_df)
+    print(normalized_data)
